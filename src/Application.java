b0316528e6e4f1802b4fc75ad435dda066c9e33b
yuwen41200 <yuwen41200@gmail.com>
2015-05-18 21:42:43 +0800

    Added Class Application

---
+++ src/Application.java
@@ -0,0 +1,9 @@
+public class Application {
+
+    public static void main(String args[]) {
+        Model model = new Model();
+        View view = new View();
+        Controller controller = new Controller();
+    }
+
+}
